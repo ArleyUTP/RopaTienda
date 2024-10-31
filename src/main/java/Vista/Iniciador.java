bce6929d07b7ede904ba716c74c45b045bb41b92
TicseTorresArleyAlberto <u23238849@utp.edu.pe>
2024-10-31 12:41:32 -0500

    Generar Tiempo en Jframe

---
+++ src/main/java/Vista/Iniciador.java
@@ -0,0 +1,9 @@
+
+package Vista;
+public class Iniciador {
+    public static void main(String[] args) {
+        Vista_bienvenida vista = new Vista_bienvenida();
+        vista.setLocationRelativeTo(null);
+        vista.setVisible(true);
+    }
+}
